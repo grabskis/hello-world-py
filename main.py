9f302d4e11081c6387cee6f2eb39e0652db90244
Wojtek <wojtek@fluxinc.ca>
2020-11-30 22:36:37 +0000

    we created multiply and modulus, Yay!

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('Yummy Tacos!')
+  
+def multiply(a, b):
+  i = 0  
+  
+  for x in range(a):
+    i += b
+
+  print(i)
+
+def modulus(a, b):
+  while not a >= b:
+    a = a - b
+    print(a)
+  
+  print(a)
+
+
+
+multiply(6000, 90)
+modulus(6, 15)
+
+
